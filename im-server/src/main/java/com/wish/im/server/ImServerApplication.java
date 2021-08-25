9c8586ffdc6da78434bec96d45036be716a7b291
shy <shy@1122>
2021-08-25 14:34:26 +0800

    删除不必要的代码

--- im-server/src/main/java/com/wish/im/server/ImServerApplication.java
+++ im-server/src/main/java/com/wish/im/server/ImServerApplication.java
@@ -1,13 +1,10 @@
 package com.wish.im.server;
 
-import com.wish.im.server.netty.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Arrays;
 
 /**
  * 描述
@@ -18,19 +15,9 @@ import java.util.Arrays;
 @SpringBootApplication
 @EnableScheduling
 public class ImServerApplication {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(ImServerApplication.class);
         springApplication.setApplicationContextClass(AnnotationConfigApplicationContext.class);
         ApplicationContext context = springApplication.run(args);
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        System.err.println(Arrays.toString(beanDefinitionNames));
-        AppConfig bean = context.getBean(AppConfig.class);
-        ThreadGroup currentGroup =
-                Thread.currentThread().getThreadGroup();
-        int noThreads = currentGroup.activeCount();
-        Thread[] lstThreads = new Thread[noThreads];
-        currentGroup.enumerate(lstThreads);
-        for (int i = 0; i < noThreads; i++)
-            System.out.println("线程号：" + i + " = " + lstThreads[i].getName());
     }
 }
